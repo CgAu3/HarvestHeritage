006ccd2e75862991b8e6a3842ae0c63cd5e8a5eb
theabab2333 <2047477146@qq.com>
2026-05-27 21:23:28 +0800

    If you want it......

--- src/main/java/me/theabab2333/harvestheritage/HarvestHeritage.java
+++ src/main/java/me/theabab2333/harvestheritage/HarvestHeritage.java
@@ -1,6 +1,5 @@
 package me.theabab2333.harvestheritage;
 
-import com.mojang.logging.LogUtils;
 import me.theabab2333.harvestheritage.init.ModBlockEntities;
 import me.theabab2333.harvestheritage.init.ModBlocks;
 import me.theabab2333.harvestheritage.init.ModConfig;
@@ -17,11 +16,12 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(HarvestHeritage.MODID)
 public class HarvestHeritage {
     public static final String MODID = "harvestheritage";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     public HarvestHeritage(IEventBus eventBus, ModContainer modContainer) {
         eventBus.addListener(this::commonSetup);
@@ -36,6 +36,9 @@ public class HarvestHeritage {
         NeoForge.EVENT_BUS.register(this);
 
         ModConfig.register(modContainer);
+
+        LOGGER.info("Hey Player, your portal opening day is over. Give me the Crash.");
+        LOGGER.info("If you want it, then you'll have to take it, as you have already known.");
     }
 
     public static Identifier of(String path) {
